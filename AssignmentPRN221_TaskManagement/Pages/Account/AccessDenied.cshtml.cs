fa59efd59652332ff0698d8c06ce5d4443ff86e7
nhhainam <110182737+nhhainam@users.noreply.github.com>
2023-03-27 20:18:18 +0700

    Auth

---
+++ AssignmentPRN221_TaskManagement/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AssignmentPRN221_TaskManagement.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- AssignmentPRN221_TaskManagement/Program.cs
+++ AssignmentPRN221_TaskManagement/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSession();
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                  .AddCookie(options =>
+                  {
+                      options.LoginPath = "~/Account/Login";
+                      options.AccessDeniedPath = "~/Account/AccessDenied";
+                  });
 
 var app = builder.Build();
 
@@ -11,11 +21,13 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 app.UseSession();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
